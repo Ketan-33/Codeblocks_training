f1910b1c1b48524d7e7ff301fd6a34267f47a533
Ketan Pardhi <ketanpardhi7@gmail.com>
2024-07-16 20:42:05 +0530

    july 16

---
+++ July_16/Recurse.java
@@ -0,0 +1,20 @@
+// package July_16;
+
+public class Recurse {
+    public static void main(String[] args){
+        print(5);
+        System.out.println(power2(5));
+    }
+    public static void print(int n){
+        if(n == 0) return;
+        System.out.println(n);
+        print(n-1);
+    }
+    public static int power2(int n) {
+        if(n==0)return 1;
+        
+        return 2*power2(n-1);
+
+
+    }
+}
